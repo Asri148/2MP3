a57de2ecd37227b9b84e250795c2b16a8d786fab
Asri148 <aditi.srinivas@outlook.com>
2023-10-10 12:23:09 -0400

    Commit 6 - Completed Assignment 1

--- main.c
+++ main.c
@@ -24,7 +24,7 @@ void placeBombs(char board[SIZE][SIZE]) {
     }
 }
 
-//Function to check if the input entered by a user as a move is within the range of the board, and in the appropriate format
+//Function to check if the input entered by a user as a move is within the range of the board
 int CheckValidMove(int x, int y) {
     return x >= 0 && x < SIZE && y >= 0 && y < SIZE;
 }
@@ -107,28 +107,35 @@ int main() {
 
     printf("Welcome to Minesweeper! Let the game begin!\n");
     while (GameNotOver == 0) {
+        //Displays the updated board to the user which includes their most recent move
         UpdateBoard(board);
         int x, y;
+        //Asks for user input which is the location the user wishes to reveal (a.k.a their move)
         printf("Enter Row and Column (0 to 9) separated by a space: ");
         scanf(" %d %d", &x, &y);
 
+        //Checks if the move made by the user is valid, if not, asks them to enter another move
         if (!CheckValidMove(x, y)) {
             printf("Invalid Move! Please enter a row and column between 0 to 9 separated by a single space.\n");
             continue;
         }
+        //Checks if the move made by the user is for a cell they have already revealed, if yes, then they are asked to make another move
         if (board[x][y] != '-' && board[x][y] != 'X') {
-            printf("This cell is already revealed!\n");
+            printf("This cell is already revealed! Please enter a new move!\n");
             continue;
         }
+        //Ends the game in the scenario where the user entered a location where there is a bomb
         if (board[x][y] == 'X') {
             printf("Game Over! You just hit a bomb.\n");
             FinalBoard(board);
             GameNotOver = 1;
+        //This statement replaces the entered location's '-' with the number of bombs found in adjacent locations 
+        //if the cell revelaed is not a bomb
         } else {
             int NumberOfBombs = countAdjacentBombs(board, x, y);
             board[x][y] = (char)(NumberOfBombs + '0');
             CellsRevealed++;
-
+            //Checks if all the cells without bombs have been revelaed. If yes, the game is over and the user won
             if (CellsRevealed == SIZE * SIZE - BOMBS) {
                 printf("Congratulations, you won!\n");
                 FinalBoard(board);

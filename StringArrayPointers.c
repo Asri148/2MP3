5fd42aa73a10a825c13b4bb2707c0738dcb6b039
Asri148 <aditi.srinivas@outlook.com>
2023-10-19 10:59:44 -0400

    Commit sudo

---
+++ StringArrayPointers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+    // Ragged array of strings
+    char *names[] = {"Anne-Marie", "Anna", "Mahmoud", "Kian", "Raouf", "Nikki"};
+
+    // Accessing and printing the elements and their lengths
+    for (int i = 0; i < 6; i ++){
+    int length = 0;
+    while (names [i][length] !='\0'){
+        length++;
+        }
+    printf ("Name %d: %s ( Length : %d) - After ’\\0’: '%c'\n", i+1, names [i], length, names[i][length]) ;
+    }
+}

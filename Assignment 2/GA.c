6f0bfd07a35df55d0d4d443495a5c7a2ac6a81d2
Asri148 <aditi.srinivas@outlook.com>
2023-11-04 21:48:40 -0400

    Part way through assignment 2

--- Assignment 2/GA.c
+++ Assignment 2/GA.c
@@ -1,12 +1,23 @@
 // Include everything necessary here 
 #include <time.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 int main(int argc, char *argv[])
 {
     // <YOUR CODE: Handle the possible errors in input data given by the user and say how to execute the code>
+    if (argc != 6)
+    {
+        printf("Usage: %s <POPULATION SIZE> <MAX GENERATIONS> <crossover rate> <mutate rate> <stop criteria>\n", argv[0]);
+        return 1;
+    }
 
     // <YOUR CODE: Assign all inputs given by the user argv[i]> like
     // POPULATION_SIZE, MAX_GENERATIONS, crossover_rate, mutate_rate, stop_criteria
+    int POPULATION_SIZE = atoi(argv[1]);
+    int MAX_GENERATIONS = atoi(argv[2]);
+    double crossover_rate = atof(argv[3]);
+    double mutate_rate = atof(argv[4]);
+    double stop_criteria = atof(argv[5]);
 
     // ###################################################################################
     // you dont need to change anything here
@@ -20,31 +31,56 @@ int main(int argc, char *argv[])
 
 
     // <YOUR CODE: Here make all the initial print outs>
-
     clock_t start_time, end_time;
     double cpu_time_used;
     start_time = clock();
 
     // <YOUR CODE: Declare all the arrays you need here>
+    // Initialize the random number generator seed
+    srand(time(NULL));
 
+    // Declare arrays for the population and fitness values
+    double population[POPULATION_SIZE][NUM_VARIABLES];
+    double fitness[POPULATION_SIZE];
+    double new_population[POPULATION_SIZE][NUM_VARIABLES];
 
     // <YOUR CODE: Call generate_population function to initialize the "population"> like:
-    // generate_population(POPULATION_SIZE, NUM_VARIABLES, population, Lbound, Ubound);
+    generate_population(POPULATION_SIZE, NUM_VARIABLES, population, Lbound, Ubound);
 
     // iteration starts here. The loop continues until MAX_GENERATIONS is reached
     // Or stopping criteria is met
+    int best_index = 0; 
     for (int generation = 0; generation < MAX_GENERATIONS; generation++)
     {
         // <YOUR CODE: Compute the fitness values using objective function for
-        // each row in "population" (each set of variables)> like:
-        // compute_objective_function(POPULATION_SIZE, NUM_VARIABLES, population, fitness);
+        compute_objective_function(POPULATION_SIZE, NUM_VARIABLES, population, fitness);
 
         // <YOUR CODE: Here implement the logic of finding best solution with minimum fitness value
         // and the stopping criteria>
+        double best_fitness = fitness[0];
+        int best_index = 0;
+        for (int i = 1; i < POPULATION_SIZE; i++)
+        {
+            if (fitness[i] < best_fitness)
+            {
+                best_fitness = fitness[i];
+                best_index = i;
+            }
+        }
+        // Print the generation number and best fitness value
+        printf("Generation %d: Best Fitness = %lf\n", generation, best_fitness);
 
+        // Check stopping criteria
+        if (best_fitness < stop_criteria)
+        {
+            printf("Stopping criteria met. Best solution found.\n");
+            break;
+        }
         // <YOUR CODE: Here call the crossover function>
+        crossover(POPULATION_SIZE, NUM_VARIABLES, fitness, new_population, population, crossover_rate);
 
         // <YOUR CODE: Here call the mutation function>
+        mutate(POPULATION_SIZE, NUM_VARIABLES, new_population, population, Lbound, Ubound, mutate_rate);
 
         // Now you have the a new population, and it goes to the beginning of loop to re-compute all again
     }
@@ -60,6 +96,15 @@ int main(int argc, char *argv[])
     // ###################################################################################
 
     // <Here print out the best solution and objective function value for the best solution like the format>
+    // Print the CPU time taken for the code
+    printf("CPU time: %f seconds\n", cpu_time_used);
 
+    // Print the best solution and objective function value for the best solution
+    printf("Best Solution:\n");
+    for (int i = 0; i < NUM_VARIABLES; i++)
+    {
+        printf("Variable %d: %lf\n", i, population[best_index][i]);
+    }
+    printf("Objective Function Value: %lf\n", fitness[best_index]);
     return 0;
 }
--- Assignment 2/functions.c
+++ Assignment 2/functions.c
@@ -1,28 +1,38 @@
 // Include everything necessary here 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-
-double generate_random(double min, double max)
-{
+double generate_random(double min, double max){
     // implement a function to return a value between min and max
+    return min + (max - min) * ((double)rand() / RAND_MAX);
 }
 
-unsigned int generate_int()
-{
-    // implement the function to return a random integer value
+unsigned int generate_int(){
+    // implement a function to return a random integer value
+    return rand();
 }
 
 // Function to initialize a random population
 void generate_population(int POPULATION_SIZE, int NUM_VARIABLES, double population[POPULATION_SIZE][NUM_VARIABLES], double Lbound[NUM_VARIABLES], double Ubound[NUM_VARIABLES])
 {
     // randomly initialize for all values in "population[i][j]""
+    srand((unsigned int)time(NULL));
+    for (int i = 0; i < POPULATION_SIZE; ++i){
+        for (int j = 0; j < NUM_VARIABLES; ++j){
+            population[i][j] = generate_random(Lbound[j], Ubound[j]);
+        }
+    }  
 }
 
 // Function to compute the objective function for each member of the population
 void compute_objective_function(int POPULATION_SIZE, int NUM_VARIABLES, double population[POPULATION_SIZE][NUM_VARIABLES], double fitness[POPULATION_SIZE])
 {
-
     /* compute "fitness[i]"" for each set of decision variables (individual) or each row in "population"
     by calling "Objective_function" */
+     for (int i = 0; i < POPULATION_SIZE; i++){
+        fitness[i] = Objective_function(NUM_VARIABLES, population[i]);
+    }
 }
 
 void crossover(int POPULATION_SIZE, int NUM_VARIABLES, double fitness[POPULATION_SIZE], double new_population[POPULATION_SIZE][NUM_VARIABLES], double population[POPULATION_SIZE][NUM_VARIABLES], double crossover_rate)
@@ -30,9 +40,41 @@ void crossover(int POPULATION_SIZE, int NUM_VARIABLES, double fitness[POPULATION
     /* Implement the logic of crossover function here based on "fitness_probs" or each set
     of decision variables (individual) or each row in "population".
     And save the new population in "new_population"*/
+     for (int i = 0; i < POPULATION_SIZE; i += 2){
+        if (generate_random(0, 1) < crossover_rate){
+            // Perform crossover for individuals with indices i and i+1
+            int crossover_point = generate_int() % NUM_VARIABLES;
+            for (int j = 0; j < crossover_point; j++){
+                new_population[i][j] = population[i][j];
+                new_population[i + 1][j] = population[i + 1][j];
+            }
+            for (int j = crossover_point; j < NUM_VARIABLES; j++){
+                new_population[i][j] = population[i + 1][j];
+                new_population[i + 1][j] = population[i][j];
+            }
+        }
+        else{
+            // If no crossover, copy parents to the new population
+            for (int j = 0; j < NUM_VARIABLES; j++){
+                new_population[i][j] = population[i][j];
+                new_population[i + 1][j] = population[i + 1][j];
+            }
+        }
+    }
 }
 
-void mutate(int POPULATION_SIZE, int NUM_VARIABLES, double new_population[POPULATION_SIZE][NUM_VARIABLES], double population[POPULATION_SIZE][NUM_VARIABLES], double Lbound[NUM_VARIABLES], double Ubound[NUM_VARIABLES], double mutate_rate)
-{
+void mutate(int POPULATION_SIZE, int NUM_VARIABLES, double new_population[POPULATION_SIZE][NUM_VARIABLES], double population[POPULATION_SIZE][NUM_VARIABLES], double Lbound[NUM_VARIABLES], double Ubound[NUM_VARIABLES], double mutate_rate){
     /*Implement the logic of mutation on "new_population" and then copy everything into "population"*/
+     for (int i = 0; i < POPULATION_SIZE; i++){
+        for (int j = 0; j < NUM_VARIABLES; j++){
+            if (generate_random(0, 1) < mutate_rate){
+                // Mutate the gene
+                new_population[i][j] = generate_random(Lbound[j], Ubound[j]);
+            }
+            else{
+                // Keep the gene unchanged
+                new_population[i][j] = population[i][j];
+            }
+        }
+    }
 }
